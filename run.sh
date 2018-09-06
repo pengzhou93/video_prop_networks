b4428aeea09184c5289e99685100a405306d66b3
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2018-09-06 14:37:18 +0800

    new file:   run.sh

---
+++ run.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+
+debug_str="import pydevd;pydevd.settrace('localhost', port=8081, stdoutToServer=True, stderrToServer=True)"
+# pydevd module path
+export PYTHONPATH=/home/shhs/Desktop/user/soft/pycharm-2018.1.4/debug-eggs/pycharm-debug-py3k.egg_FILES
+
+insert_debug_string()
+{
+    file=$1
+    line=$2
+    debug_string=$3
+    debug=$4
+
+    value=`sed -n ${line}p "$file"`
+
+    if [ "$value" != "$debug_string" ] && [ "$debug" = debug ]
+    then
+    echo "++Insert $debug_string in line_${line}++"
+
+    sed "${line}s/^/\n/" -i $file
+    sed -i "${line}s:^:${debug_string}:" "$file"
+    fi
+}
+
+delete_debug_string()
+{
+    file=$1
+    line=$2
+    debug_string=$3
+
+    value=`sed -n ${line}p "$file"`
+    if [ "$value" = "$debug_string" ]
+    then
+    echo "--Delete $debug_string in line_${line}--"
+    sed "${line}d" -i "$file"
+    fi
+}
+
+make_build_dir()
+{
+    buildPath=$1
+    rebuild=$2
+
+    if [ -d "$buildPath" ] && [ "$rebuild" = rebuild ]
+    then
+    rm -rf "$buildPath"
+    fi
+
+    if [ ! -d "$buildPath" ]
+    then
+    mkdir -p "$buildPath"
+    fi
+}
+
+build_caffe()
+{
+    rebuild=$1
+
+    cd lib/caffe
+    buildPath=build
+    make_build_dir $buildPath $rebuild
+
+    cd build
+    cmake ..
+    make -j8
+    cd ../..
+}
+
+if [ "$1" = build ]
+then
+#   ./run.sh build norebuild
+#   python: python3.5
+#   The version of python must be compatible with boost.
+    rebuild=$2
+    build_caffe "$rebuild"
+
+
+fi
